483a48c42c0d1d157b56891a7f4be2fccb1e2156
Alter0 <hackplayerz@users.noreply.github.com>
2018-12-01 16:59:56 +0900

    AddFile
    2018-12-01

---
+++ 2018-12-01/Assets/Script/ArrowController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ArrowController : MonoBehaviour {
+	void FixedUpdate () 
+	{
+		if(transform.position.y < -5f){
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ 2018-12-01/Assets/Script/Playercontroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Playercontroller : MonoBehaviour {
+
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		if(Input.GetButton("Horizontal")){
+			transform.Translate(20 * Input.GetAxis("Horizontal") * Time.deltaTime,0,0);
+		}
+	}
+}
