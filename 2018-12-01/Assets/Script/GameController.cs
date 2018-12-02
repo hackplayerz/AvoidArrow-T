c1ce818d2a29e48354406b9063ae2031bd853fa5
Alter0 <hackplayerz@users.noreply.github.com>
2018-12-02 17:49:38 +0900

    Add files via upload

--- 2018-12-01/Assets/Script/GameController.cs
+++ 2018-12-01/Assets/Script/GameController.cs
@@ -6,8 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
-{
+public class GameController : MonoBehaviour {
     public int Coin = 0;
     public int Point = 0;
     public int Spawn = 0;
@@ -19,41 +18,34 @@ public class GameController : MonoBehaviour
     float SpawnTime = 2f;
     float AppleSpawnRemain = 0f;
 
-    void Update()
-    {
+    void Update () {
         AppleSpawnRemain += Time.deltaTime;
-
-        if (AppleSpawnRemain > 2.3f)
-        {
-            Instantiate(Apple, new Vector2(Random.Range(-7, 7), 6f), Quaternion.identity);
-            AppleSpawnRemain = 0f;
+        if (isGameOver != true) {
+            if (AppleSpawnRemain > 2.3f) {
+                Instantiate (Apple, new Vector2 (Random.Range (-7, 7), 6f), Quaternion.identity);
+                AppleSpawnRemain = 0f;
+            }
         }
     }
-    void FixedUpdate()
-    {
-        if (isGameOver == true)
-        {
+    void FixedUpdate () {
+        if (isGameOver == true) {
             return;
         }
         SpawnRemain += Time.deltaTime;
         // Block OverFlow
-        if (Spawn > 300000)
-        {
+        if (Spawn > 300000) {
             Spawn = 0;
         }
 
-        if (SpawnRemain > SpawnTime)
-        {
-            if (Spawn % 9 == 0 && Spawn > 1)
-            {
-                if (SpawnTime < .5)
-                {
+        if (SpawnRemain > SpawnTime) {
+            if (Spawn % 9 == 0 && Spawn > 1) {
+                if (SpawnTime < .5) {
                     SpawnTime = .5f;
                 }
                 SpawnTime -= .3f;
             }
 
-            Instantiate(Arrow, new Vector2(Random.Range(-7, 7), 6f), Quaternion.identity);
+            Instantiate (Arrow, new Vector2 (Random.Range (-7, 7), 6f), Quaternion.identity);
             SpawnRemain = 0;
         }
 
