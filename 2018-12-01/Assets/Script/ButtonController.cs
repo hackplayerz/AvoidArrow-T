808da3453c0af5ddc18c4892a8d53053012a3421
Alter0 <hackplayerz@users.noreply.github.com>
2018-12-02 17:39:06 +0900

    UpLoad

---
+++ 2018-12-01/Assets/Script/ButtonController.cs
@@ -0,0 +1,20 @@
+﻿/*
+ *		CopyRight : Dev.Alter0
+ *		https://github.com/hackplayerz
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+	GameObject player = null;
+	void Start () {
+		player = GameObject.FindWithTag ("Player");
+	}
+	public void OnLeftButton () {
+		player.transform.Translate (-2, 0, 0);
+	}
+	public void OnRightButton () {
+		player.transform.Translate (2, 0, 0);
+	}
+}
--- 2018-12-01/Assets/Script/Playercontroller.cs
+++ 2018-12-01/Assets/Script/Playercontroller.cs
@@ -3,30 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(BoxCollider2D))]
-public class Playercontroller : MonoBehaviour
-{
+[RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (BoxCollider2D))]
+public class Playercontroller : MonoBehaviour {
+    public Rigidbody2D mRigidbody = null;
     public int PlayerHP = 10;
     GameObject HPGage = null;
     GameController GameController = null;
-    Rigidbody2D mRigidbody = null;
 
-    void Start()
-    {
-        mRigidbody = GetComponent<Rigidbody2D>();
-        GameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
-        HPGage = GameObject.Find("HP");
-        Reset();
+    void Start () {
+        mRigidbody = GetComponent<Rigidbody2D> ();
+        GameController = GameObject.FindWithTag ("GameController").GetComponent<GameController> ();
+        HPGage = GameObject.Find ("HP");
+        Reset ();
     }
 
-    void Update()
-    {
-        if (GameController.isGameOver != true)
-        {
-            if (Input.GetButton("Horizontal"))
-            {
-                transform.Translate(20 * Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
+    void Update () {
+        if (GameController.isGameOver != true) {
+            if (Input.GetButton ("Horizontal")) {
+                transform.Translate (20 * Input.GetAxis ("Horizontal") * Time.deltaTime, 0, 0);
             }
         }
     }
@@ -34,26 +29,22 @@ public class Playercontroller : MonoBehaviour
     /// <summary>
     /// Reset Player Setting
     /// </summary>
-    void Reset()
-    {
-        mRigidbody.transform.position = new Vector2(0f, -3.6f);
+    void Reset () {
+        mRigidbody.transform.position = new Vector2 (0f, -3.6f);
         PlayerHP = 10;
     }
     /// <summary>
     /// PLayer Hit Event ,if PLayerHP is smaller then 0, Set GameOver
     /// </summary>
-    public void Hit()
-    {
-        Debug.Log("Hit");
+    public void Hit () {
+        Debug.Log ("Hit");
         PlayerHP--;
-        HPGage.GetComponent<Image>().fillAmount -= .1f;
-        if (PlayerHP <= 0)
-        {
+        HPGage.GetComponent<Image> ().fillAmount -= .1f;
+        if (PlayerHP <= 0) {
             GameController.isGameOver = true;
         }
     }
-    public void GetCoin()
-    {
+    public void GetCoin () {
         GameController.Coin++;
     }
 }
